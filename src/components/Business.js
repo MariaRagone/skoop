9476f67eeb7f083dc79916e9fc8f7b354be82d05
Maria Ragone <maria@ragone.net>
2024-10-07 22:27:47 -0400

    search bar is finding restaurants

--- src/components/Business.js
+++ src/components/Business.js
@@ -14,19 +14,31 @@ const businessDetails = [
   },
 ];
 
-function Business ({src, name, address, city, state, zipcode, category, rating, reviewCount}) {
+function Business({
+  src,
+  name,
+  address,
+  city,
+  state,
+  zipcode,
+  category,
+  rating,
+  reviewCount,
+}) {
   return (
-<div className="business">
+    <div className="business">
       <h2>{name}</h2>
       <img src={src} alt={name} />
       <p>{address}</p>
-      <p>{city}, {state} {zipcode}</p>
+      <p>
+        {city}, {state} {zipcode}
+      </p>
       <span>{category}</span>
       <span>Rating:{rating}</span>
-      
+
       <p>Number of Reviews:{reviewCount}</p>
     </div>
   );
-};
+}
 
 export default Business;
--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,25 +1,24 @@
 import React from "react";
 import Business from "./Business";
 
-
-function BusinessList (props) {
-    return (
-        <div className="business-list">
-          {props.businesses.map((business, index) => (
-            <Business 
-              key={index}
-              src={business.src}
-              name={business.name}
-              address={business.address}
-              city={business.city}
-              state={business.state}
-              zipcode={business.zipcode}
-              category={business.category}
-              rating={business.rating}
-              reviewCount={business.reviewCount}
-            />
-          ))}
-        </div>
-      );
-    };
+function BusinessList(props) {
+  return (
+    <div className="business-list">
+      {props.businesses.map((business, index) => (
+        <Business
+          key={index}
+          src={business.src}
+          name={business.name}
+          address={business.address}
+          city={business.city}
+          state={business.state}
+          zipcode={business.zipcode}
+          category={business.category}
+          rating={business.rating}
+          reviewCount={business.reviewCount}
+        />
+      ))}
+    </div>
+  );
+}
 export default BusinessList;
