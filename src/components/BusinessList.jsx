4b22b9abd6e71f1ae55280e1f152358f95d84aa9
mragone <maria.ragone@samaritanministries.org>
2024-10-31 08:33:37 -0400

    removed unneeded code

--- src/components/BusinessList.jsx
+++ src/components/BusinessList.jsx
@@ -1,24 +1,10 @@
 import React from "react";
 import Business from "./Business";
 
-function BusinessList(props) {
+function BusinessList({ businesses }) {
   return (
     <div className="business-list">
-      {/* {props.businesses.map((business, index) => (
-        <Business
-          key={index}
-          src={business.src}
-          name={business.name}
-          address={business.address}
-          city={business.city}
-          state={business.state}
-          zipcode={business.zipcode}
-          category={business.category}
-          rating={business.rating}
-          reviewCount={business.reviewCount}
-        />
-      ))} */}
-      {props.businesses.map((business) => {
+      {businesses.map((business) => {
         return <Business business={business} key={business.name} />;
       })}
     </div>
