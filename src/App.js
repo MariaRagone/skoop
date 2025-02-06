4baf7bdc96d060a8d9a2920c129cfa4c358c3ec5
mragone <maria.ragone@samaritanministries.org>
2025-02-06 08:22:10 -0500

    refactored styling for sorting options to move them into their own component

--- src/App.js
+++ src/App.js
@@ -53,7 +53,6 @@ function App() {
       <Header />
       <SearchBar />
       <BusinessList businesses={businesses} />
-      {/* <BusinessPage /> */}
     </div>
   );
 }
--- src/components/SearchBar/SortingOption.jsx
+++ src/components/SearchBar/SortingOption.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import "./SortingOption.css";
+
 const sortingOptions = ["Best Match", "Highest Rated", "Most Reviewed"];
 
 const SortingOption = ({ setSelectedOption }) => {
   return (
-    <ul>
+    <ul className="sorting-options">
       {sortingOptions.map((option) => (
         <li
+          className="sorting-options-item"
           key={option}
           onClick={() => {
             setSelectedOption(option);
